53f6cdd8c362fedaa89344c6840fb2b85f452ec0
AdrianRubioT <adrianalumnosmx@gmail.com>
2018-11-14 18:03:40 +0100

    acceso a la BD y crear una <ul>

--- Tarea11/server/public/listar/investigadoresListar.js
+++ Tarea11/server/public/listar/investigadoresListar.js
@@ -1,8 +1,9 @@
-$.getJSON( "http://localhost:3000/convivencia ", function( data ) {
+$.getJSON( "http://localhost:3000/convivencia", function( data ) {
   var items = [];
+  /* console.log(data); */
     $.each( data, function( key, val ) {
-	console.log(key);
-    items.push( "<li id='" + key + "' class='list-group-item'>" + val.nombre + "</li>" );
+/*   console.log(key); */
+    items.push( "<li id='" + key + "' class='list-group-item'>" + val.alumno + "</li>" );
   });
  
   $( "<ul/>", {
