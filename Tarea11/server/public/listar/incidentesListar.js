d3bc813115ef57f12a7ea680a6b81fc1fa84e9eb
AdrianRubioT <adrianalumnosmx@gmail.com>
2018-11-14 19:05:46 +0100

    cambio de nombre

--- Tarea11/server/public/listar/incidentesListar.js
+++ Tarea11/server/public/listar/incidentesListar.js
@@ -1,9 +1,9 @@
 $.getJSON( "http://localhost:3000/convivencia", function( data ) {
   var items = [];
-  /* console.log(data); */
+   console.log(data); 
     $.each( data, function( key, val ) {
 /*   console.log(key); */
-    items.push( "<li id='" + key + "' class='list-group-item'>" + val.alumno + "</li>" );
+    items.push( "<li id='" + val._id + "' class='list-group-item'>" + val.alumno + "</li>" );
   });
  
   $( "<ul/>", {
