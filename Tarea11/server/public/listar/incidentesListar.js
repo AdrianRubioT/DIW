abf463cf41f08e5bd7ffc737dd8fdc6c87c56386
AdrianRubioT <adrianalumnosmx@gmail.com>
2018-11-21 19:11:38 +0100

    comentado la linea console log

--- Tarea11/server/public/listar/incidentesListar.js
+++ Tarea11/server/public/listar/incidentesListar.js
@@ -1,6 +1,6 @@
 $.getJSON( "http://localhost:3000/convivencia", function( data ) {
   var items = [];
-   console.log(data); 
+   /* console.log(data); */ 
     $.each( data, function( key, val ) {
 /*   console.log(key); */
     items.push( "<li id='" + val._id + "' class='list-group-item'>" + val.alumno + "</li>" );
@@ -11,3 +11,5 @@ $.getJSON( "http://localhost:3000/convivencia", function( data ) {
     html: items.join( "" )
   }).appendTo( "#divLista" );
 });
+
+
