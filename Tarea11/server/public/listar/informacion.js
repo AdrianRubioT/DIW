e871ca35cfa44bee18bd715e1925a424c45e3dfd
AdrianRubioT <adrianalumnosmx@gmail.com>
2018-12-03 16:54:00 +0100

    pasando los campos de castigo a string

--- Tarea11/server/public/listar/informacion.js
+++ Tarea11/server/public/listar/informacion.js
@@ -1,8 +1,22 @@
 /* js para que contendra los castigos y las tipificaciones con texto para traducirlo de la base de datos que estan en booleano */
 
 /* var y = "holamundo";
-
-
 function hola() {
     return y;
 } */
+
+function castigo1() {
+    return "Compareixença davant el cap d'estudis/director";
+}
+
+function castigo2(aparell) {
+    return "Retirada d'aparells electrònics " + aparell;
+}
+
+function castigo3(diaInicio, diaFinal) {
+    return "Privació del temps de recreació els dies: " + diaInicio + " fins a " + diaFinal;
+}
+
+function castigo4(tarea, dies, ) {
+    return "Realizació de les següents tasques educadores: " + tarea + " els dies " + dies + " i amb l'orari " + 
+}
