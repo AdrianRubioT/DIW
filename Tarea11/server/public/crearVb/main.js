d2b5b3d6873e36819e4bfe5caf49df0a0cc212ca
AdrianRubioT <adrianalumnosmx@gmail.com>
2018-12-12 19:14:01 +0100

    comenzando funcion para modificar el input del html

--- Tarea11/server/public/crearVb/main.js
+++ Tarea11/server/public/crearVb/main.js
@@ -1,6 +1,35 @@
-function onSubmit( form ){
-    
-    var data = JSON.stringify( $(form).serializeArray() ); //  <-----------
-    console.log( data );
-    return false; //don't submit
-  }
+function onSubmit(form) {
+
+  var data = JSON.stringify($(form).serializeArray()); //  <-----------
+  console.log(data);
+  return false; //don't submit
+}
+
+
+
+function cargar() {
+
+  document.getElementById("leve").addEventListener("click", tFlipFLop, false);
+  document.getElementById("grave").addEventListener("click", tFlipFLop, false);
+
+}
+
+function tFlipFLop(event) {
+  
+  /* linea para obtener el id del boton que genera la accion */
+  /* console.log(event.originalTarget.id); */
+
+  document.getElementById("nivel").value = event.originalTarget.id;
+
+
+
+
+  
+}
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", cargar, false);
