7fdb34e321c10e8ff9336592361fcccf6c81ea61
AdrianRubioT <adrianalumnosmx@gmail.com>
2019-01-10 17:19:48 +0100

    Quitado la funcion de prueba

--- Tarea11/server/public/listar/informacion.js
+++ Tarea11/server/public/listar/informacion.js
@@ -1,10 +1,5 @@
 /* js para que contendra los castigos y las tipificaciones con texto para traducirlo de la base de datos que estan en booleano */
 
-/* var y = "holamundo";
-function hola() {
-    return y;
-} */
-
 function castigo1() {
     return "Compareixença davant el cap d'estudis/director";
 }
